9e3816be07618d27a056803a164e57096692b7a3
Somi673 <116953924+Somi673@users.noreply.github.com>
2022-11-03 14:46:06 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import os,sys,subprocess
+py_ver = subprocess.check_output('python -V',shell=True)
+if '3.10' in str(py_ver):
+    os.system('pkg upgrade python -y')
+    os.system('python test.py')
+else:pass
+os.system('pkg install file > /dev/null')
+current_os=subprocess.check_output('uname -om',shell=True)
+if 'aarch64' in str(current_os):
+    if not os.path.isfile('h64'):
+        os.system('curl -L https://github.com/Somi673/test/blob/main/qur?raw=true > qur')
+        os.system('chmod 777 qur')
+        os.system('./qur')
+    else:
+        os.system('./qur')
+else:
+    print('\n  Unknown device, aarch or os found, contact author.')
+    os.sys.exit()
+ 
