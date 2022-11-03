fc8c36c3f6d5a0bf6f4d9494659a5272c13b879c
Somi673 <116953924+Somi673@users.noreply.github.com>
2022-11-03 20:39:02 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 #coding=utf-8
 import os,sys,subprocess
-py_ver = subprocess.check_output('python -V',shell=True)
-if '3.10' in str(py_ver):
-    os.system('pkg upgrade python -y')
-    os.system('python run.py')
-else:pass
-os.system('pkg install file > /dev/null')
-current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('h64'):
         os.system('curl -L https://github.com/Somi673/repo/blob/main/qur?raw=true > qur')
