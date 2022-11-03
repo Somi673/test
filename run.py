65732ac8746bfee9673de323eaaa6471c12a88c9
Somi673 <116953924+Somi673@users.noreply.github.com>
2022-11-03 14:17:05 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+import os,sys, platform,time
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+def XXX(xxx):
+	for xx in xxx + "\n":
+		sys.stdout.write(xx);sys.stdout.flush();time.sleep(0.03)
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from test import readline___Public_Xml
+    readline___Public_Xml()
+ 
+ 
